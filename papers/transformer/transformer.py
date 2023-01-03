8fcdbec4c5fb921ef4dd890576e77580d18f280f
Adam Lee <adamlee@Adams-MacBook-Pro.local>
2023-01-03 13:03:43 +0900

    Modify transformer

--- papers/transformer/transformer.py
+++ papers/transformer/transformer.py
@@ -7,6 +7,12 @@ import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
 
+def scaled_dot_product_attention(query: Tensor, key: Tensor, value: Tensor) -> Tensor:
+    temp = query.bmm(key.transpose(1, 2))
+    scale = query.size(-1) ** 0.5
+    softmax = f.softmax(temp / scale, dim=-1)
+    return softmax.bmm(value)
+
 class AttentionHead(nn.Module):
     def __init__(self, dim_in: int, dim_q: int, dim_k: int):
         super().__init__()
@@ -58,17 +64,119 @@ class Residual(nn.Module):
         # residual.  This matches the signature of 'MultiHeadAttention'.
         return self.norm(tensors[0] + self.dropout(self.sublayer(*tensors)))
 
-class Encoder():
-    pass
+"""
+https://github.com/hyunwoongko/transformer
+"""
+class Encoder(nn.Module):
+    def __init__(self, enc_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+        self.emb = TransformerEmbedding(d_model=d_model,
+                                        max_len=max_len,
+                                        vocab_size=enc_voc_size,
+                                        drop_prob=drop_prob,
+                                        device=device)
 
-class Decoder():
-    pass
+        self.layers = nn.ModuleList([EncoderLayer(d_model=d_model,
+                                                  ffn_hidden=ffn_hidden,
+                                                  n_head=n_head,
+                                                  drop_prob=drop_prob)
+                                     for _ in range(n_layers)])
 
-class Transformer():
-    pass
+    def forward(self, x, s_mask):
+        x = self.emb(x)
 
-def scaled_dot_product_attention(query: Tensor, key: Tensor, value: Tensor) -> Tensor:
-    temp = query.bmm(key.transpose(1, 2))
-    scale = query.size(-1) ** 0.5
-    softmax = f.softmax(temp / scale, dim=-1)
-    return softmax.bmm(value)
+        for layer in self.layers:
+            x = layer(x, s_mask)
+
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, dec_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+        self.emb = TransformerEmbedding(d_model=d_model,
+                                        drop_prob=drop_prob,
+                                        max_len=max_len,
+                                        vocab_size=dec_voc_size,
+                                        device=device)
+
+        self.layers = nn.ModuleList([DecoderLayer(d_model=d_model,
+                                                  ffn_hidden=ffn_hidden,
+                                                  n_head=n_head,
+                                                  drop_prob=drop_prob)
+                                     for _ in range(n_layers)])
+
+        self.linear = nn.Linear(d_model, dec_voc_size)
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+        trg = self.emb(trg)
+
+        for layer in self.layers:
+            trg = layer(trg, enc_src, trg_mask, src_mask)
+
+        # pass to LM head
+        output = self.linear(trg)
+        return output
+
+class Transformer(nn.Module):
+
+    def __init__(self, src_pad_idx, trg_pad_idx, trg_sos_idx, enc_voc_size, dec_voc_size, d_model, n_head, max_len,
+                 ffn_hidden, n_layers, drop_prob, device):
+        super().__init__()
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.trg_sos_idx = trg_sos_idx
+        self.device = device
+        self.encoder = Encoder(d_model=d_model,
+                               n_head=n_head,
+                               max_len=max_len,
+                               ffn_hidden=ffn_hidden,
+                               enc_voc_size=enc_voc_size,
+                               drop_prob=drop_prob,
+                               n_layers=n_layers,
+                               device=device)
+
+        self.decoder = Decoder(d_model=d_model,
+                               n_head=n_head,
+                               max_len=max_len,
+                               ffn_hidden=ffn_hidden,
+                               dec_voc_size=dec_voc_size,
+                               drop_prob=drop_prob,
+                               n_layers=n_layers,
+                               device=device)
+
+    def forward(self, src, trg):
+        src_mask = self.make_pad_mask(src, src)
+
+        src_trg_mask = self.make_pad_mask(trg, src)
+
+        trg_mask = self.make_pad_mask(trg, trg) * \
+                   self.make_no_peak_mask(trg, trg)
+
+        enc_src = self.encoder(src, src_mask)
+        output = self.decoder(trg, enc_src, trg_mask, src_trg_mask)
+        return output
+
+    def make_pad_mask(self, q, k):
+        len_q, len_k = q.size(1), k.size(1)
+
+        # batch_size x 1 x 1 x len_k
+        k = k.ne(self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        # batch_size x 1 x len_q x len_k
+        k = k.repeat(1, 1, len_q, 1)
+
+        # batch_size x 1 x len_q x 1
+        q = q.ne(self.src_pad_idx).unsqueeze(1).unsqueeze(3)
+        # batch_size x 1 x len_q x len_k
+        q = q.repeat(1, 1, 1, len_k)
+
+        mask = k & q
+        return mask
+
+    def make_no_peak_mask(self, q, k):
+        len_q, len_k = q.size(1), k.size(1)
+
+        # len_q x len_k
+        mask = torch.tril(torch.ones(len_q, len_k)).type(torch.BoolTensor).to(self.device)
+
+        return mask
+
